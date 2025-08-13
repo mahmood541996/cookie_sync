f3b0aea5a5ebb3778476a6ef389d8870608edecb
@mahmoud.abdulrazek <mahmoud.abdulrazek@troylab.org>
2025-08-13 13:21:21 +0300

    Introduced interface for CookieSync and its impl

--- lib/cookie_sync.dart
+++ lib/cookie_sync.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/services.dart';
 
-class CookieSync {
-  static const MethodChannel _channel = MethodChannel('cookie_sync');
+abstract interface class CookieSync {
+  factory CookieSync() = _CookieSync;
 
   /// Forces all cookies in memory to be written to persistent storage.
+  Future<void> syncCookies();
+}
+
+final class _CookieSync implements CookieSync {
+  _CookieSync() : this._(MethodChannel('cookie_sync'));
+
+  _CookieSync._(this._channel);
+
+  final MethodChannel _channel;
+
   Future<void> syncCookies() async {
     await _channel.invokeMethod('syncCookies');
   }
